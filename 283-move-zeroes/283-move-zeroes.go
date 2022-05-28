f79c716df49d4584bc6efcba38b2c1c8cf1fe5bb
Alan Zhan <rsesst60313@gmail.com>
2022-05-28 22:57:03 +0800

    Time: 57 ms (16.27%), Space: 7.9 MB (5.31%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.go
+++ 283-move-zeroes/283-move-zeroes.go
@@ -1,12 +1,13 @@
 func moveZeroes(nums []int)  {
     n := 0
-    for i := 0; i < len(nums); i++ {
+    numsLen := len(nums)
+    for i := 0; i < numsLen; i++ {
         if nums[i] != 0 {
             nums[n] = nums[i]
             n++
         }
     }
-    for n < len(nums) {
+    for n < numsLen {
         nums[n] = 0
         n++
     }
