60114825acbdb404a91d838b8e8322a42bb69c5b
Alan Zhan <rsesst60313@gmail.com>
2022-06-03 01:25:50 +0800

    Time: 0 ms (100.00%), Space: 1.9 MB (82.67%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.go
+++ 20-valid-parentheses/20-valid-parentheses.go
@@ -4,13 +4,14 @@ func isValid(s string) bool {
     }
     stack := make([]rune, 0)
     for _, v := range s {
-        if v == '(' || v == '{' || v == '[' {
+        if v == '(' || v == '[' || v == '{' {
             stack = append(stack, v)
-        } else if ((v == ']') && len(stack) > 0 && stack[len(stack)-1] == '[') ||
-			((v == ')') && len(stack) > 0 && stack[len(stack)-1] == '(') ||
-			((v == '}') && len(stack) > 0 && stack[len(stack)-1] == '{') {
-			stack = stack[:len(stack)-1]
-		} else {
+        } else if len(stack) > 0 &&
+        ((v == ')' && stack[len(stack) - 1] == '(') ||
+        (v == ']' && stack[len(stack) - 1] == '[')||
+         (v == '}' && stack[len(stack) - 1] == '{')) {
+            stack = stack[:len(stack) - 1]
+        } else {
             return false
         }
     }
