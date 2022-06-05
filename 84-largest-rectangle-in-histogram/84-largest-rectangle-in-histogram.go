db5f037ea4b838c797edfb47b0db35e1a82d7b9b
Alan Zhan <rsesst60313@gmail.com>
2022-06-05 20:11:36 +0800

    Time: 165 ms (51.52%), Space: 10.1 MB (51.52%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.go
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.go
@@ -1,24 +1,23 @@
 type rect struct {
-    width int
-    height int
+    width   int
+    height  int
 }
 
 func largestRectangleArea(heights []int) int {
     stack := []rect{}
-    answer := 0
+    max := 0
     heights = append(heights, 0)
     for _, height := range heights {
         accumulatedWidth := 0
         
-        for len(stack) != 0 && stack[len(stack) - 1].height > height {
+        for len(stack) != 0 && stack[len(stack) - 1].height >= height {
             accumulatedWidth += stack[len(stack) - 1].width
-            temp := stack[len(stack) - 1].height * accumulatedWidth
-            if temp > answer {
-                answer = temp
+            if max < stack[len(stack) - 1].height * accumulatedWidth {
+                max = stack[len(stack) - 1].height * accumulatedWidth
             }
             stack = stack[:len(stack) - 1]
         }
         stack = append(stack, rect{accumulatedWidth + 1, height})
     }
-    return answer
+    return max
 }
