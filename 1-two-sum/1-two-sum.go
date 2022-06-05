8b4d36760b543e45fc6fdf079d19ed575f439493
Alan Zhan <rsesst60313@gmail.com>
2022-06-06 01:31:38 +0800

    Time: 9 ms (60.26%), Space: 4.3 MB (25.43%) - LeetHub

--- 1-two-sum/1-two-sum.go
+++ 1-two-sum/1-two-sum.go
@@ -1,13 +1,11 @@
 func twoSum(nums []int, target int) []int {
-    for i, a := range nums {
-        for j, b := range nums {
-            if i == j {
-                continue
-            }
-            if a + b == target {
-                return []int {i, j}
-            }
+    maps := make(map[int]int, len(nums))
+
+    for i, num := range nums {
+        if index, isExist := maps[num]; isExist {
+            return []int{index, i}
         }
+        maps[target - num] = i
     }
     return nil
 }
