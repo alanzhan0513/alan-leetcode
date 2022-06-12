59313d490ef15f610992fe9a24bfa00b910f5ce8
Alan Zhan <rsesst60313@gmail.com>
2022-06-12 22:53:40 +0800

    Time: 3 ms (46.65%), Space: 2 MB (70.28%) - LeetHub

---
+++ 50-powx-n/50-powx-n.go
@@ -0,0 +1,15 @@
+func myPow(x float64, n int) float64 {
+    if n == 0 {
+        return 1
+    }
+
+    if n < 0 {
+        return 1 / myPow(x, -n)
+    }
+    temp := myPow(x, n / 2)
+    ans := temp * temp
+    if n % 2 == 1 {
+        ans *= x
+    }
+    return ans
+}
