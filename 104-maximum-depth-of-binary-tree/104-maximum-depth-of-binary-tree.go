cb92ab1633138d415f858952e599621df07afbaa
Alan Zhan <rsesst60313@gmail.com>
2022-06-13 01:19:12 +0800

    Time: 2 ms (88.90%), Space: 4.2 MB (49.40%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.go
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.go
@@ -6,17 +6,26 @@
  *     Right *TreeNode
  * }
  */
+var depth int
+var ans int
 func maxDepth(root *TreeNode) int {
+    depth = 1
+    ans = 0
+    calculateDepth(root)
+    return ans
+}
+
+func calculateDepth(root *TreeNode) {
     if root == nil {
-        return 0
+        return
     }
     
-    left := maxDepth(root.Left) + 1
-    right := maxDepth(root.Right) + 1
-    
-    if left >= right {
-        return left
-    } else {
-        return right
+    if ans < depth {
+        ans = depth
     }
+    
+    depth++
+    calculateDepth(root.Left)
+    calculateDepth(root.Right)
+    depth--
 }
