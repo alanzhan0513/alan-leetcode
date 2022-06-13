f2a03ced715ceb952853c7cd9a588e348d467440
Alan Zhan <rsesst60313@gmail.com>
2022-06-13 23:17:44 +0800

    Time: 2 ms (50.08%), Space: 2.3 MB (73.01%) - LeetHub

--- 78-subsets/78-subsets.go
+++ 78-subsets/78-subsets.go
@@ -1,25 +1,19 @@
-var length int
-var stack []int
-var ans [][]int
-
 func subsets(nums []int) [][]int {
-    length = len(nums)
-    stack = []int{}
-    ans = [][]int{}
-    recursive(0, nums)
-    return ans
+    return recursive(nums, 0, [][]int{[]int{}})
 }
 
-func recursive(i int, nums []int) {
-    if length == i {
-        temp := make([]int, len(stack))
-        copy(temp, stack)
-        ans = append(ans, temp)
-        return
+func recursive(nums []int, i int, ans [][]int) [][]int {
+    if i >= len(nums) {
+        return ans
     }
     
-    recursive(i + 1, nums)
-    stack = append(stack, nums[i])
-    recursive(i + 1, nums)
-    stack = stack[:len(stack) - 1]
+    temp := make([][]int, len(ans))
+    for j := 0; j < len(temp); j++ {
+        temp[j] = make([]int, len(ans[j]))
+        copy(temp[j], ans[j])
+        temp[j] = append(temp[j], nums[i])
+    }
+    
+    ans = append(ans, temp...)
+    return recursive(nums, i + 1, ans)
 }
