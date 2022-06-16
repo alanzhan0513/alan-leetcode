80a95ef22b4eb2743b2553ac12ab952d6939d966
Alan Zhan <rsesst60313@gmail.com>
2022-06-17 00:13:52 +0800

    Time: 5 ms (28.95%), Space: 3.2 MB (27.75%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.go
+++ 22-generate-parentheses/22-generate-parentheses.go
@@ -2,11 +2,12 @@ func generateParenthesis(n int) []string {
     if n == 0 {
         return []string{""}
     }
+    
     ans := []string{}
     for i := 1; i <= n; i++ {
         A := generateParenthesis(i - 1)
         B := generateParenthesis(n - i)
-        
+
         for _, a := range A {
             for _, b := range B {
                 ans = append(ans, "(" + a + ")" + b)
