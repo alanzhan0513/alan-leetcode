1b4270243ac7068a3d6a9793bae49998f4d3b425
Alan Zhan <rsesst60313@gmail.com>
2022-06-19 23:19:02 +0800

    Time: 19 ms (46.88%), Space: 6.6 MB (36.59%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.go
@@ -0,0 +1,39 @@
+func findOrder(numCourses int, prerequisites [][]int) []int {
+    to := make([][]int, numCourses)
+    inDeg := make([]int, numCourses)
+    for _, prerequisite := range prerequisites {
+        ai := prerequisite[0]
+        bi := prerequisite[1]
+        if to[bi] == nil {
+            to[bi] = []int{ai}
+        } else {
+            to[bi] = append(to[bi], ai)
+        }
+        inDeg[ai]++
+    }
+    queue := []int{}
+    for i := 0; i < numCourses; i++ {
+        if inDeg[i] == 0 {
+            queue = append(queue, i)
+        }
+    }
+    lessons := []int{}
+    ansQueue := []int{}
+    for len(queue) != 0 {
+        x := queue[0]
+        queue = queue[1:len(queue)]
+        ansQueue = append(ansQueue, x)
+        lessons = append(lessons, x)
+        for _, y := range to[x] {
+            inDeg[y]--
+            if inDeg[y] == 0 {
+                queue = append(queue, y)
+            }
+        }
+    }
+    if len(lessons) == numCourses {
+        return ansQueue
+    } else {
+        return []int{}
+    }
+}
