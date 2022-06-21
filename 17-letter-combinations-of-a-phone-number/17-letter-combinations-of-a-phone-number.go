ae3f6c47afbb10816eb4798c57cf79229fb38b90
Alan Zhan <rsesst60313@gmail.com>
2022-06-22 01:00:42 +0800

    Time: 0 ms (100.00%), Space: 2 MB (90.65%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.go
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.go
@@ -1,41 +1,33 @@
+var maps map[rune][]rune
 func letterCombinations(digits string) []string {
-	return letterCombinations1(digits)
+    if digits == "" {
+        return []string{}
+    }
+    
+    maps = make(map[rune][]rune, 8)
+    maps['2'] = []rune{'a','b','c'}
+    maps['3'] = []rune{'d','e','f'}
+    maps['4'] = []rune{'g','h','i'}
+    maps['5'] = []rune{'j','k','l'}
+    maps['6'] = []rune{'m','n','o'}
+    maps['7'] = []rune{'p','q','r','s'}
+    maps['8'] = []rune{'t','u','v'}
+    maps['9'] = []rune{'w','x','y','z'}
+
+    ans := []string{}
+    recursive(digits, 0, "", &ans)
+    return ans
 }
 
-func letterCombinations1(digits string) []string {
-	if len(digits) == 0 {
-		return []string{}
-	}
+func recursive(digits string, index int, current string, ans *[]string) {
+    if index == len(digits) {
+        *ans = append(*ans, current)
+        return
+    }
 
-	output, index, cur := []string{}, 0, ""
-	hash := map[byte]string{
-		'2': "abc",
-		'3': "def",
-		'4': "ghi",
-		'5': "jkl",
-		'6': "mno",
-		'7': "pqrs",
-		'8': "tuv",
-		'9': "wxyz",
-	}
-	backtrack(hash, &output, digits, cur, index)
-	return output
-}
-
-func backtrack(hash map[byte]string, output *[]string, digits string, cur string, index int) {
-	if index > len(digits) {
-		return
-	}
-
-	if len(cur) == len(digits) {
-		*output = append(*output, cur)
-		return
-	}
-
-	symbols := hash[digits[index]]
-	for _, v := range symbols {
-		cur += string(v)
-		backtrack(hash, output, digits, cur, index+1)
-		cur = cur[:len(cur)-1]
-	}
+    for _, v := range maps[rune(digits[index])] {
+        current += string(v)
+        recursive(digits, index + 1, current, ans)
+        current = current[:len(current) - 1]
+    }
 }
