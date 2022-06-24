c8f96b0549bf3717bcac3ef103dfaaaddcf72fde
Alan Zhan <rsesst60313@gmail.com>
2022-06-25 00:40:38 +0800

    Time: 3 ms (13.73%), Space: 2 MB (7.84%) - LeetHub

--- 433-minimum-genetic-mutation/433-minimum-genetic-mutation.go
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.go
@@ -1,44 +1,51 @@
 func minMutation(start string, end string, bank []string) int {
-	mp := make(map[string]bool)
-	vis := make(map[string]bool)
-	for _, v := range bank {
+    queue := []string{}
+    visited := make(map[string]bool, 0)
     
-		mp[v] = true
-	}
-	ans := math.MaxInt32
-	dirs := []byte("ACGT")
-	var dfs func(cur string, step int)
-	dfs = func(cur string, step int) {
+    if contains(bank, end) && start == end {
+        return -1
+    }
     
-		if cur == end {
-    
-			ans = step
-			return
-		}
-		vis[cur] = true
-		for i := 0; i < 8; i++ {
-    
-			for _, v := range dirs {
-    
-				temp := []byte(cur)
-				if v != temp[i] {
-    
-					temp[i] = v
-					cnt := string(temp)
-					if ans != math.MaxInt32 || !mp[cnt] || vis[cnt] {
-    
-						continue
-					}
-					dfs(string(temp), step+1)
-				}
-			}
-		}
-		vis[cur] = false
-	}
-	dfs(start, 0)
-	if ans == math.MaxInt32 {
-    
-		return -1
-	}
-	return ans
+    queue = append(queue, start)
+    visited[start] = true
+    ans := 0
+    for len(queue) != 0 {
+        length := len(queue)
+        for i := 0; i < length; i++ {
+            current := queue[0]
+            queue = queue[1:]
+            
+            if current == end {
+                return ans
+            }
+            
+            for _, b := range bank {
+                if canMutate(b, current) && !visited[b] {
+                    queue = append(queue, b)
+                    visited[b] = true
+                }
+            }
+        }
+        ans++
+    }
+    return -1
+}
+
+func canMutate(str1, str2 string) bool {
+    counter := 0
+    for i := 0; i < len(str1); i++ {
+        if str1[i] != str2[i] {
+            counter++
+        }
+    }
+    return counter == 1
+}
+
+func contains(bank []string, tofind string) bool {
+    for _, b := range bank {
+        if tofind == b {
+            return true
+        }
+    }
+    return false
 }
