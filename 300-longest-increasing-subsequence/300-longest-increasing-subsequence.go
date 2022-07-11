07c1654994ec2923d841ba9d891c92ca2199fd9b
Alan Zhan <rsesst60313@gmail.com>
2022-07-11 23:52:37 +0800

    Time: 82 ms (26.19%), Space: 3.7 MB (61.16%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.go
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.go
@@ -1,9 +1,13 @@
 func lengthOfLIS(nums []int) int {
     length := len(nums)
     dp := make([]int, length)
-    ans := 0
+    
     for i := 0; i < length; i++ {
         dp[i] = 1
+    }
+    
+    ans := 0
+    for i := 0; i < length; i++ {
         for j := 0; j < i; j++ {
             if nums[i] > nums[j] {
                 dp[i] = max(dp[i], dp[j] + 1)
