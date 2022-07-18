8a25964ff5e4537a9244601d2d8e53f8389bd0f3
Alan Zhan <rsesst60313@gmail.com>
2022-07-18 22:11:35 +0800

    Time: 12 ms (34.65%), Space: 5.1 MB (63.37%) - LeetHub

---
+++ 337-house-robber-iii/337-house-robber-iii.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rob(root *TreeNode) int {
+    var dfs func(root *TreeNode) [2]int
+	dfs = func(root *TreeNode) [2]int { //0选 1不选
+		if root == nil {
+			return [2]int{0, 0}
+		}
+		l := dfs(root.Left)
+		r := dfs(root.Right)
+		selected := root.Val + l[1] + r[1]
+		notSelected := max(l[0], l[1]) + max(r[0], r[1])
+		return [2]int{selected, notSelected}
+	}
+	val := dfs(root)
+	return max(val[0], val[1])
+}
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
