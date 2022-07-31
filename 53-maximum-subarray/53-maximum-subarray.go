41b21c46de2214f3d658b952adef4cad67fa49b8
Alan Zhan <rsesst60313@gmail.com>
2022-08-01 01:35:32 +0800

    Time: 146 ms (56.77%), Space: 10.2 MB (13.58%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.go
+++ 53-maximum-subarray/53-maximum-subarray.go
@@ -2,11 +2,9 @@ func maxSubArray(nums []int) int {
     length := len(nums)
     dp := make([]int, length)
     dp[0] = nums[0]
-    for i := 1; i < length; i++ {
-        dp[i] = max(nums[i] + dp[i - 1], nums[i])
-    }
     ans := dp[0]
     for i := 1; i < length; i++ {
+        dp[i] = max(nums[i] + dp[i - 1], nums[i])
         ans = max(ans, dp[i])
     }
     return ans
