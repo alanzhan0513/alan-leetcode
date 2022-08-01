ff4c8ebb2afbd3e27b61f484c5f850168a0bff58
Alan Zhan <rsesst60313@gmail.com>
2022-08-01 23:31:18 +0800

    Time: 10 ms (45.03%), Space: 4 MB (7.23%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.go
+++ 152-maximum-product-subarray/152-maximum-product-subarray.go
@@ -1,35 +1,38 @@
 func maxProduct(nums []int) int {
-    max := nums[0]
-    min := nums[0]
-    ans := max
+    length := len(nums)
+    dpMax := make([]int, length)
+    dpMin := make([]int, length)
+    dpMax[0] = nums[0]
+    dpMin[0] = nums[0]
     
-    for i := 1; i < len(nums); i++ {
-        n := nums[i]
-        tempMax := getMax(n, max * n, min * n)
-        min = getMin(n, max * n, min * n)
-        
-        max = tempMax
-        ans = getMax(max, ans)
+    for i := 1; i < length; i++ {
+        values := []int{dpMax[i - 1] * nums[i], dpMin[i - 1] * nums[i], nums[i]}
+        dpMax[i] = max(values...)
+        dpMin[i] = min(values...)
     }
     
+    ans := dpMax[0]
+    for i := 1; i < length; i++ {
+        ans = max(ans, dpMax[i])
+    }
     return ans
 }
 
-func getMax(nums ...int) int {
-    max := nums[0]
-    for _, n := range nums {
-        if n > max {
-            max = n
+func max(v ...int) int {
+    max := v[0]
+    for i := 0; i < len(v); i++ {
+        if v[i] > max {
+            max = v[i]
         }
     }
     return max
 }
 
-func getMin(nums ...int) int {
-    min := nums[0]
-    for _, n := range nums {
-        if n < min {
-            min = n
+func min(v ...int) int {
+    min := v[0]
+    for i := 0; i < len(v); i++ {
+        if v[i] < min {
+            min = v[i]
         }
     }
     return min
