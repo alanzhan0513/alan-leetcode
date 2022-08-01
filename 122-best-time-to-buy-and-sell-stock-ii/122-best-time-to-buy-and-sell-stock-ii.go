53252fb09c565524a19a322e67e8fd70b72c0f12
Alan Zhan <rsesst60313@gmail.com>
2022-08-02 02:04:46 +0800

    Time: 12 ms (16.21%), Space: 4.5 MB (5.28%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.go
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.go
@@ -1,9 +1,24 @@
 func maxProfit(prices []int) int {
-    maxprofit := 0
+    // move index to 1
+    prices = append([]int{ 0 }, prices...)
+    
+    dp := make([][]int, len(prices))
+    dp[0] = make([]int, 2)
+    dp[0][1] = -1 << 32
+    dp[0][0] = 0
+    
     for i := 1; i < len(prices); i++ {
-        maxprofit += max(0, prices[i] - prices[i - 1])
+        dp[i] = make([]int, 2)
+        dp[i][0] = -1 << 32
+        dp[i][1] = -1 << 32
+        
+        dp[i][1] = max(dp[i][1], dp[i - 1][0] - prices[i])
+        dp[i][0] = max(dp[i][0], dp[i - 1][1] + prices[i])
+        for j := 0; j < 2; j++ {
+            dp[i][j] = max(dp[i][j], dp[i - 1][j])
+        }
     }
-    return maxprofit
+    return dp[len(prices) - 1][0]
 }
 
 func max(a, b int) int {
