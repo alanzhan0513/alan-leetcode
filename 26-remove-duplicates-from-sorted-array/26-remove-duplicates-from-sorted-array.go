d33d0aae200b7a38e3c359c30561fe18667af15b
Alan Zhan <rsesst60313@gmail.com>
2022-08-21 01:54:08 +0800

    Time: 13 ms (56.40%), Space: 4.4 MB (38.27%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.go
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.go
@@ -1,7 +1,7 @@
 func removeDuplicates(nums []int) int {
-    n := 0
-    for i := 0; i < len(nums); i++ {
-        if n == 0 || nums[i] != nums[i -1] {
+    n := 1
+    for i := 1; i < len(nums); i++ {
+        if nums[i] != nums[i - 1] {
             nums[n] = nums[i]
             n++
         }
