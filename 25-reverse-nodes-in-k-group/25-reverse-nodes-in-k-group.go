34d31cacb14739d5b1114dfeed85d87b813a78ac
Alan Zhan <rsesst60313@gmail.com>
2022-08-28 13:35:23 +0800

    Time: 6 ms (61.14%), Space: 3.5 MB (88.08%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.go
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.go
@@ -6,33 +6,30 @@
  * }
  */
 func reverseKGroup(head *ListNode, k int) *ListNode {
-    // 1. group
-    // 2. reverse
-    // 3. update tail and head's ListNode
-    sentinel := &ListNode{0, head}
-    last := sentinel
+    // 1. grouping and find head and tail of group
+    // 2. reverse in the group
+    // 3. update head and tail link
+
+    sentinal := &ListNode{}
+    last := sentinal
     for head != nil {
-        end := getEnd(head, k)
+        end := findEnd(head, k)
         if end == nil {
             break
         }
-        
-        // 2. reverse ListNode in group
         nextGroupHead := end.Next
-        reverse(head, nextGroupHead)
         
-        // 3
+        reverseList(head, nextGroupHead)
         last.Next = end
         head.Next = nextGroupHead
-        
         
         last = head
         head = nextGroupHead
     }
-    return sentinel.Next
+    return sentinal.Next
 }
 
-func getEnd(head *ListNode, k int) *ListNode {
+func findEnd(head *ListNode, k int) *ListNode {
     for head != nil {
         k--
         if k == 0 {
@@ -43,13 +40,13 @@ func getEnd(head *ListNode, k int) *ListNode {
     return nil
 }
 
-func reverse(head *ListNode, end *ListNode) {
+func reverseList(head, tail *ListNode) {
     last := head
     head = head.Next
-    for head != end {
-        nextHead := head.Next
+    for head != tail {
+        headNext := head.Next
         head.Next = last
         last = head
-        head = nextHead
+        head = headNext
     }
 }
